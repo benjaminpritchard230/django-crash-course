24e1ff92a863e38eb860c168d480b09b9760fd2c
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2022-10-26 21:33:29 +0100

    Learning django

--- movies/urls.py
+++ movies/urls.py
@@ -21,4 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('movies/', views.movies),
     path('', views.home),
+    path('movies/<int:id>/', views.detail),
+    path('movies/add/', views.add),
+    path('movies/delete/<int:id>/', views.delete),
 ]
